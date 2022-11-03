ef6295e9f0c776e050e991c86f373fc92ac835ee
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-11-04 02:47:42 +0600

    post api and get api add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
+const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
@@ -13,4 +14,42 @@ app.use(express.json());
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aegbdxu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
+async function run(){
+
+    try{
+        await client.connect();
+        console.log("DB Connect");
+        const workCollection = client.db('workList').collection('work');
+
+        //data post
+        app.post('/work', async(req, res) =>{
+            const newWork = req.body;
+            const result = await workCollection.insertOne(newWork);
+            res.send(result);
+        })
+          
+        // get all data 
+        app.get('/work', async(req, res) =>{
+            const query = {};
+            const cursor = workCollection.find(query);
+            const works = await cursor.toArray();
+            res.send(works);
+        })
+
+    }
+
+    finally{
+
+    }
+}
+
+
+run().catch(console.dir);
+
+app.get('/', (req, res) =>{
+    res.send("Hello from Data List")
+})
+
+app.listen(port, () =>{
+    console.log("Listening port", port);
+})
